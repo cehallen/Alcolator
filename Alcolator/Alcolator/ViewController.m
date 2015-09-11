13e10f2b85c90ef7f9a8cb7fa424f5e08d20522c
Chris Allen <cehallen@gmail.com>
2015-09-11 14:47:08 -0400

    Add new subclass

--- Alcolator/Alcolator/ViewController.m
+++ Alcolator/Alcolator/ViewController.m
@@ -10,10 +10,6 @@
 
 @interface ViewController ()
 
-@property (weak, nonatomic) IBOutlet UITextField *beerPercentTextField;
-@property (weak, nonatomic) IBOutlet UISlider *beerCountSlider;
-@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
-
 @end
 
 @implementation ViewController
@@ -29,15 +25,55 @@
 }
 
 - (IBAction)textFieldDidChange:(UITextField *)sender {
+    NSString * enteredText = sender.text;
+    float enteredNumber = [enteredText floatValue];
+    if (enteredNumber == 0) {
+        sender.text = nil;
+    }
 }
 
 - (IBAction)sliderValueDidChange:(UISlider *)sender {
+    NSLog(@"slider value changed to %f", sender.value);
+    [self.beerPercentTextField resignFirstResponder];
 }
 
 - (IBAction)buttonPressed:(UIButton *)sender {
+    [self.beerPercentTextField resignFirstResponder];
+    
+    // calculate how much alcohol in beers
+    int numberOfBeers = self.beerCountSlider.value;
+    int ouncesInOneBeerGlass = 12;
+    float alcoholPercentageOfBeer = [self.beerPercentTextField.text floatValue] / 100;
+    float ouncesOfAlcoholPerBeer = ouncesInOneBeerGlass * alcoholPercentageOfBeer;
+    float ouncesOfAlcoholTotal = ouncesOfAlcoholPerBeer * numberOfBeers;
+
+    // calculate equivalent amount of wine
+    float ouncesInOneWineGlass = 5;
+    float alcoholPercentageOfWine = 0.13;
+    float ouncesOfAlcoholPerWineGlass = ouncesInOneWineGlass * alcoholPercentageOfWine;
+    float numberOfWineGlassesForEquivalentAlcoholAmount = ouncesOfAlcoholTotal / ouncesOfAlcoholPerWineGlass;
+    
+    // decide to use "beer/beers" and "glass/glasses"
+    NSString *beerText;
+    if (numberOfBeers == 1) {
+        beerText = NSLocalizedString(@"beer", @"singular beer");
+    } else {
+        beerText = NSLocalizedString(@"beers", @"plural of beer");
+    }
+    NSString *wineText;
+    if (numberOfWineGlassesForEquivalentAlcoholAmount == 1) {
+        wineText = NSLocalizedString(@"glass", @"singular glass");
+    } else {
+        wineText = NSLocalizedString(@"glasses", @"plural of glass");
+    }
+    
+    // generate the result text and display to label
+    NSString *resultText = [NSString stringWithFormat:NSLocalizedString(@"%d %@ (with %.2f%% alcohol) contains as much alcohol as %.1f %@ of wine.", nil), numberOfBeers, beerText, [self.beerPercentTextField.text floatValue], numberOfWineGlassesForEquivalentAlcoholAmount, wineText];
+    self.resultLabel.text = resultText;
 }
 
 - (IBAction)tapGestureDidFire:(UITapGestureRecognizer *)sender {
+    [self.beerPercentTextField resignFirstResponder];
 }
 
 
---
+++ Alcolator/Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Christopher Allen on 9/11/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
