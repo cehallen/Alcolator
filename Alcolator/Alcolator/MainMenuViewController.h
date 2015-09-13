7e3b250acfea36b90850b0ab05f912c86dcf2a38
Chris Allen <cehallen@gmail.com>
2015-09-13 10:18:44 -0400

    Add navbar and segues

---
+++ Alcolator/Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Christopher Allen on 9/13/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainMenuViewController : ViewController
+
+@end
---
+++ Alcolator/Alcolator/MainMenuViewController.m
@@ -0,0 +1,19 @@
+//
+//  MainMenuViewController.m
+//  Alcolator
+//
+//  Created by Christopher Allen on 9/13/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import "MainMenuViewController.h"
+
+@interface MainMenuViewController ()
+
+@end
+
+@implementation MainMenuViewController
+
+
+
+@end
